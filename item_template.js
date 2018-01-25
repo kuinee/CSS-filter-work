f15aab163cd3ab5a85ed2ffe93de6a47bfc69647
kuinne <15768653694@163.com>
2018-01-25 09:53:28 +0800

    first commit

---
+++ item_template.js
@@ -0,0 +1,36 @@
+
+var ITEM_TEMPLATE=[
+    '<div id="work_',
+    null,
+    '" class="',
+    null,
+    '"><div><div class="blur" style="background-image: url(',
+    null,
+    ');"></div><div class="cover" style="background-image: url(',
+    null,
+    ');"></div></div><div class="info"><div><span class="title">',
+    null,
+    '</span><span class="duration"><i></i>0:30</span></div><div><span class="date">',
+    null,
+    '</span><i></i><span class="status"><i></i>',
+    null,
+    '</span></div></div><div class="mask"></div><div class="mask-content"><a href="javascript:detail(\'',
+    null,
+    '\')" class="todo"></a><a href="javascript:download(\'',
+    null,
+    '\')" class="download"></a><a href="javascript:deleteWork(\'',
+    null,
+    '\')" class="delete"></a></div></div>'
+]
+
+
+    1 id
+    3 success/fail/build
+    5 cover
+    7 cover
+    9 title
+    11 ceateAt
+    13 3->合成/完成/失败
+    15 id
+    17 id
+    19 id
